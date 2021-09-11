bbea47b5490235f547c200d9f08bec6e623a4255
DizerX <jadoyahya@gmail.com>
2021-09-11 23:17:53 +0300

    Fixed a visual bug for mobile

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,21 @@ const popup = document.querySelector(".popup");
 const closeBtn = document.querySelector(".close_popup");
 const eye = document.querySelector(".eye");
 const back = document.querySelector(".back");
+const body = document.querySelector("body");
 
 signinBtn.addEventListener("click", () => {
   popup.classList.toggle("visible");
+  body.classList.toggle("scroll");
 });
 
 closeBtn.addEventListener("click", () => {
   popup.classList.toggle("visible");
+  body.classList.toggle("scroll");
 });
 
 back.addEventListener("click", () => {
   popup.classList.toggle("visible");
+  body.classList.toggle("scroll");
 });
 
 // to prevent modal from showing for 100ms when page load
